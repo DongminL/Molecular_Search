a0d281d4f4d9659df9a68506e38e67ab6a84c9f9
DongminL <milkskfk12@gmail.com>
2024-03-16 04:00:23 +0900

    project setting

---
+++ src/main/java/com/example/molecularsearch/MolecularSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.molecularsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolecularSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MolecularSearchApplication.class, args);
+    }
+
+}
