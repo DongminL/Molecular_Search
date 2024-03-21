e552af64d33da81ea99bc3238a2d3f13b90c7d20
DongminL <milkskfk12@gmail.com>
2024-03-21 14:04:51 +0900

    네이버 프로필 정보 가져오기

--- app/src/main/java/com/example/canchem/MainActivity.kt
+++ app/src/main/java/com/example/canchem/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.canchem
 
 import android.os.Bundle
+import android.os.Message
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -9,7 +10,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.canchem.databinding.ActivityMainBinding
 import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
 import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 
 
 class MainActivity : AppCompatActivity() {
@@ -37,6 +41,33 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.btnNaverLogin.setOnClickListener {
+
+            val nidProfileCallback = object : NidProfileCallback<NidProfileResponse> {
+                override fun onSuccess(response: NidProfileResponse) {
+                    val userId = response.profile?.id
+                    val email = response.profile?.email
+                    val mobile = response.profile?.mobile
+
+                    Toast.makeText(this@MainActivity, "네이버 로그인 성공\n" +
+                            "user id : ${userId}\n" +
+                            "email: ${email}\n" +
+                            "mobile : ${mobile}", Toast.LENGTH_SHORT).show()
+                }
+                override fun onFailure(httpState : Int, message: String) {
+                    val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                    val errorDesc = NaverIdLoginSDK.getLastErrorDescription()
+
+                    Toast.makeText(this@MainActivity, "네이버 로그인 실패\n" +
+                            "Error Code : ${errorCode}\n" +
+                            "Error Description : ${errorDesc}", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(errorCode: Int, message: String) {
+                    onFailure(errorCode, message)
+                }
+
+            }
+
             val oauthLoginCallback = object : OAuthLoginCallback {
                 override fun onSuccess() {
                     // 네이버 로그인 인증이 성공했을 때 수행할 코드 추가
@@ -46,25 +77,26 @@ class MainActivity : AppCompatActivity() {
                     val type = NaverIdLoginSDK.getTokenType().toString()    // 토큰 타입
                     val state = NaverIdLoginSDK.getState().toString()   // 로그인 인스턴트의 현재 상태
 
-                    Log.i("Login Data", accessToken)
-                    Log.i("Login Data", refreshToken)
-                    Log.i("Login Data", expiresAt)
-                    Log.i("Login Data", type)
-                    Log.i("Login Data", state)
-                }
+                    Toast.makeText(this@MainActivity, "Access Token : ${accessToken}", Toast.LENGTH_SHORT).show()
 
+                    NidOAuthLogin().callProfileApi(nidProfileCallback)
+                }
                 override fun onFailure(httpStatus: Int, message: String) {
                     val errorCode = NaverIdLoginSDK.getLastErrorCode().code
                     val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
                     Toast.makeText(this@MainActivity,"errorCode:$errorCode, errorDesc:$errorDescription",Toast.LENGTH_SHORT).show()
                 }
-
                 override fun onError(errorCode: Int, message: String) {
                     onFailure(errorCode, message)
                 }
             }
 
             NaverIdLoginSDK.authenticate(this, oauthLoginCallback)  // 토큰 가져오기
+        }
+
+        fun naverLogout () {
+            NaverIdLoginSDK.logout()
+            Toast.makeText(this@MainActivity, "네이버 로그아웃 성공", Toast.LENGTH_SHORT).show()
         }
     }
 }
