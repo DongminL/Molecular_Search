11bd20041fa736b0c693632ab0140a9cd99bf069
DongminL <milkskfk12@gmail.com>
2024-03-29 03:55:58 +0900

    create jwt

---
+++ src/main/java/com/example/molecularsearch/dto/JwtDto.java
@@ -0,0 +1,23 @@
+package com.example.molecularsearch.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class JwtDto {
+
+    private String accessToken; // 접근 토큰
+    private String refreshToken;    // 갱신 토큰
+    private String grantType;   // 토큰 타입
+    private Long expiredAt; // 만료 기간
+
+    @Builder
+    public JwtDto(String accessToken, String refreshToken, String grantType, Long expiredAt) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.grantType = grantType;
+        this.expiredAt = expiredAt;
+    }
+}
--- src/main/java/com/example/molecularsearch/jwt/JwtProvider.java
+++ src/main/java/com/example/molecularsearch/jwt/JwtProvider.java
@@ -1,17 +1,41 @@
 package com.example.molecularsearch.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtProvider implements InitializingBean {
 
     private Key key;
+    private final String secret;
+    private final long expiration_time;
+
+    public JwtProvider(@Value("${jwt.secret}") String secret,
+                       @Value("${jwt.expiration-time}") long expiration_time) {
+        this.secret = secret;
+        this.expiration_time = expiration_time;
+    }
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
+    /* Token 생성 (유저 키 값, 만료 기간) */
+    public String createToken(String subject, Date expiredAt) {
+        return Jwts.builder()
+                .setSubject(subject)    // 유저 키값 명시
+                .setExpiration(expiredAt)   // 만료 기간 설정
+                .signWith(this.key, SignatureAlgorithm.HS512)   // 암호화 알고리즘 설정
+                .compact();
     }
 }
