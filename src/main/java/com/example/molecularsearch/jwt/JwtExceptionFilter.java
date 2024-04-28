4aad0b5d8f27e1f15dea8be195116e6a41a928e5
DongminL <milkskfk12@gmail.com>
2024-04-29 02:05:39 +0900

    global error handling

--- src/main/java/com/example/molecularsearch/jwt/JwtExceptionFilter.java
+++ src/main/java/com/example/molecularsearch/jwt/JwtExceptionFilter.java
@@ -1,6 +1,6 @@
 package com.example.molecularsearch.jwt;
 
-import com.example.molecularsearch.dto.ErrorResponse;
+import com.example.molecularsearch.dto.ErrorDto;
 import com.example.molecularsearch.exception.CustomException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 @Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
@@ -26,10 +25,9 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);    // 다음 filter인 JwtFilter로 값 전달
         } catch (CustomException e) {
             // 에러 응답 메시지 생성
-            ErrorResponse errorRes = ErrorResponse.builder()
-                    .timestamp(LocalDateTime.now().toString())
-                    .staus(e.getErrorCode().getStatus().value())
-                    .error(e.getErrorCode().getStatus().name())
+            ErrorDto errorRes = ErrorDto.builder()
+                    .staus(e.getErrorCode().getStatus())
+                    .error(e.getErrorCode().getError())
                     .message(e.getErrorCode().getMessage())
                     .build();
             
