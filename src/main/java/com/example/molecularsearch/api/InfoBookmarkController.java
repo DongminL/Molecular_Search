fccc5548055ce3fc620632c5a91d10caf2fb5c29
DongminL <milkskfk12@gmail.com>
2024-05-17 23:15:31 +0900

    400에러 처리

--- src/main/java/com/example/molecularsearch/api/InfoBookmarkController.java
+++ src/main/java/com/example/molecularsearch/api/InfoBookmarkController.java
@@ -1,6 +1,8 @@
 package com.example.molecularsearch.api;
 
 import com.example.molecularsearch.dto.InfoBookmarkDto;
+import com.example.molecularsearch.exception.CustomException;
+import com.example.molecularsearch.exception.ErrorCode;
 import com.example.molecularsearch.service.InfoBookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +19,20 @@ public class InfoBookmarkController {
 
     /* 즐겨찾기 추가 */
     @PostMapping("/api/save/bookmark/{chemId}")
-    public ResponseEntity<?> addInfoFav(@PathVariable String chemId) {
-        infoBookmarkService.saveInfoFav(chemId);
+    public ResponseEntity<?> addInfoBookmark(@PathVariable String chemId) {
+        infoBookmarkService.saveInfoBookmark(chemId);
         return ResponseEntity.ok("즐겨찾기 추가 성공");
     }
 
     /* 즐겨찾기 리스트 */
     @GetMapping("/api/search/bookmark")
-    public ResponseEntity<?> showInfoFav() {
+    public ResponseEntity<?> showInfoBookmark() {
         return ResponseEntity.ok(infoBookmarkService.getBookmarkList());
     }
 
     /* 즐겨찾기 단일 삭제 */
     @DeleteMapping("/api/delete/bookmark/{infoBookmarkId}")
-    public ResponseEntity<String> editInfoFav(@PathVariable Long infoBookmarkId) {
+    public ResponseEntity<String> editInfoBookmark(@PathVariable Long infoBookmarkId) {
         infoBookmarkService.deleteInfoBookmark(infoBookmarkId);
 
         return ResponseEntity.ok("검색기록 삭제 완료");
@@ -38,8 +40,14 @@ public class InfoBookmarkController {
 
     /* 즐겨찾기 편집을 통해 삭제 */
     @DeleteMapping("/api/edit/bookmark")
-    public ResponseEntity<String> editInfoFav(@RequestBody Map<String, List<InfoBookmarkDto>> mapInfoFavDtos) {
-        infoBookmarkService.editInfoBookmark(mapInfoFavDtos.get("infoFavList"));
+    public ResponseEntity<String> editInfoBookmark(@RequestBody Map<String, List<InfoBookmarkDto>> mapInfoBookmarkDtos) {
+        try {
+            mapInfoBookmarkDtos.get("infoBookmarkList");
+        } catch (NullPointerException e) {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        }
+
+        infoBookmarkService.editInfoBookmark(mapInfoBookmarkDtos.get("infoBookmarkList"));
 
         return ResponseEntity.ok("검색기록 삭제 완료");
     }
