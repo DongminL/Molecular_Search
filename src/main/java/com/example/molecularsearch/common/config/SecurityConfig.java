325c4d9284b3d25f576de351f4527b5be6332157
DongminL <milkskfk12@gmail.com>
2024-05-24 20:44:12 +0900

    Redis에 토큰 여부 체크

--- src/main/java/com/example/molecularsearch/common/config/SecurityConfig.java
+++ src/main/java/com/example/molecularsearch/common/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.molecularsearch.common.config;
 
+import com.example.molecularsearch.jwt.repository.TokensRepository;
 import com.example.molecularsearch.jwt.web.filter.JwtExceptionFilter;
 import com.example.molecularsearch.jwt.web.filter.JwtFilter;
 import com.example.molecularsearch.jwt.web.handler.JwtHandler;
@@ -23,6 +24,7 @@ public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final JwtHandler jwtHandler;
+    private final TokensRepository tokensRepository;
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -38,10 +40,10 @@ public class SecurityConfig {
                         httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(jwtHandler)
                                 .accessDeniedHandler(jwtHandler))
                 // JwtFilter를 Security 로직에 등록
-                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)    // JwtFilter를 UsernamePasswordAuthenticationFilter보다 먼저 실행
+                .addFilterBefore(new JwtFilter(jwtProvider, tokensRepository), UsernamePasswordAuthenticationFilter.class)    // JwtFilter를 UsernamePasswordAuthenticationFilter보다 먼저 실행
                 // URL 권한 설정 */
                 .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests.requestMatchers("/api/login/naver", "/api/login/google", "/api/login/reissue").permitAll()  // 로그인, 토큰 갱신 요청은 누구든지 허용
+                        authorizeRequests.requestMatchers("/api/login/naver", "/api/login/google").permitAll()  // 로그인 요청은 누구든지 허용
                                 .anyRequest().authenticated()   // 그외 다른 요청들은 토큰 인증해야함
                 );
 
