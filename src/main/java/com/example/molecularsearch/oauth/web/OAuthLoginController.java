831be404757ba28c8b42245a6ca17fc586e43a34
DongminL <milkskfk12@gmail.com>
2024-06-01 17:24:51 +0900

    주석 추가 및 사용하지 않은 api 삭제

--- src/main/java/com/example/molecularsearch/oauth/web/OAuthLoginController.java
+++ src/main/java/com/example/molecularsearch/oauth/web/OAuthLoginController.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 public class OAuthLoginController {
@@ -17,26 +15,29 @@ public class OAuthLoginController {
     private final OAuthLoginService oAuthLoginService;
     private final JwtService jwtService;
 
+    /* 네이버 계정으로 로그인 */
     @PostMapping("/api/login/naver")
     public ResponseEntity<?> loginNaver(@RequestBody NaverUserDto naverUserDto) {
         return ResponseEntity.ok(oAuthLoginService.login(naverUserDto));
     }
 
+    /* 구글 계정으로 로그인 */
     @PostMapping("/api/login/google")
     public ResponseEntity<?> loginGoogle(@RequestBody GoogleUserDto googleUserDto) {
         return ResponseEntity.ok(oAuthLoginService.login(googleUserDto));
     }
 
     /* 만료된 Access Token 갱신 */
-    @PatchMapping("/api/login/reissue")
-    public ResponseEntity<?> reissueToken(@RequestHeader("Authorization") String token) {
-        String accessToken = jwtService.getHeaderToken(token);  // Header 값에서 Bearer 값 제외
+//    @PatchMapping("/api/login/reissue")
+//    public ResponseEntity<?> reissueToken(@RequestHeader("Authorization") String token) {
+//        String accessToken = jwtService.getHeaderToken(token);  // Header 값에서 Bearer 값 제외
+//
+//        Map<String, String> newAccessToken = jwtService.reissueAccessToken(accessToken);
+//
+//        return  ResponseEntity.ok(newAccessToken);
+//    }
 
-        Map<String, String> newAccessToken = jwtService.reissueAccessToken(accessToken);
-
-        return  ResponseEntity.ok(newAccessToken);
-    }
-
+    /* 로그아웃 */
     @DeleteMapping("/api/logout")
     public ResponseEntity<?> logout(@RequestHeader("Authorization") String token) {
         oAuthLoginService.logout(token);
