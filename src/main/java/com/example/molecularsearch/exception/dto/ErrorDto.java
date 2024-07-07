6d9dbedc80308b47f04e16d644fd96a26f934a8d
DongminL <milkskfk12@gmail.com>
2024-07-07 18:44:56 +0900

    변수명 오타, staus -> status

--- src/main/java/com/example/molecularsearch/exception/dto/ErrorDto.java
+++ src/main/java/com/example/molecularsearch/exception/dto/ErrorDto.java
@@ -9,13 +9,13 @@ import java.time.LocalDateTime;
 public class ErrorDto {
 
     private final String timestamp = LocalDateTime.now().toString();  // 현재 시간
-    private final int staus;  // HttpStatus 코드
+    private final int status;  // HttpStatus 코드
     private final String error;   // HttpStatus 이름
     private final String message; // 에러 응답 메시지
 
     @Builder
-    public ErrorDto(int staus, String error, String message) {
-        this.staus = staus;
+    public ErrorDto(int status, String error, String message) {
+        this.status = status;
         this.error = error;
         this.message = message;
     }
