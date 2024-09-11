3e5e92cf16ab5fad5bdba60c1409fc0a27ca0f88
DongminL <milkskfk12@gmail.com>
2024-09-11 23:59:13 +0900

    토큰 응답 DTO로 변환하는 메소드 추가

--- src/main/java/com/example/molecularsearch/jwt/web/dto/JwtDto.java
+++ src/main/java/com/example/molecularsearch/jwt/web/dto/JwtDto.java
@@ -15,4 +15,13 @@ public class JwtDto {
     private String refreshToken;    // 갱신 토큰
     private String grantType;   // 토큰 타입
     private Long expiredAt; // 만료 시간 (ms)
+
+    /* JwtDto -> TokenResponse */
+    public TokenResponse toResponse() {
+        return TokenResponse.builder()
+                .accessToken(this.accessToken)
+                .grantType(this.grantType)
+                .expiredAt(this.expiredAt)
+                .build();
+    }
 }
