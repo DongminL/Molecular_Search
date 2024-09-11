d37236846f4d3054e9da461363c890f82404de24
DongminL <milkskfk12@gmail.com>
2024-09-11 23:54:06 +0900

    토큰 응답 DTO

---
+++ src/main/java/com/example/molecularsearch/jwt/web/dto/TokenResponse.java
@@ -0,0 +1,17 @@
+package com.example.molecularsearch.jwt.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken; // 접근 토큰
+    private String grantType;   // 토큰 타입
+    private Long expiredAt; // 만료 시간 (ms)
+}
