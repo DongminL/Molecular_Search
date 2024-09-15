38a3246b7f09dfa10ceb23dcd74db34da6cabf71
DongminL <milkskfk12@gmail.com>
2024-09-15 21:08:54 +0900

    REST Docs 설정을 위한 상위 클래스

---
+++ src/test/java/com/example/molecularsearch/common/documentation/RestDocsSetting.java
@@ -0,0 +1,32 @@
+package com.example.molecularsearch.common.documentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureRestDocs
+public class RestDocsSetting {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    /* MockMvc Rest Docs 초기 설정 */
+    @BeforeEach
+    void setUp(@Autowired RestDocumentationContextProvider restDocumentation,
+               @Autowired WebApplicationContext webApplicationContext) {
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))   // REST Docs의 기본 설정 적용
+                .build();
+    }
+}
