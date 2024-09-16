ad1c2d965d0c2f4debe68e063e7034b13c28edf6
DongminL <milkskfk12@gmail.com>
2024-09-16 21:46:46 +0900

    JWT 인증 필터 Mocking

--- src/test/java/com/example/molecularsearch/common/documentation/RestDocsSetting.java
+++ src/test/java/com/example/molecularsearch/common/documentation/RestDocsSetting.java
@@ -1,11 +1,14 @@
 package com.example.molecularsearch.common.documentation;
 
+import com.example.molecularsearch.jwt.web.filter.JwtExceptionFilter;
+import com.example.molecularsearch.jwt.web.filter.JwtFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,6 +22,12 @@ public class RestDocsSetting {
 
     protected MockMvc mockMvc;
     protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockBean
+    private JwtFilter jwtFilter;
+
+    @MockBean
+    private JwtExceptionFilter jwtExceptionFilter;
 
     /* MockMvc Rest Docs 초기 설정 */
     @BeforeEach
