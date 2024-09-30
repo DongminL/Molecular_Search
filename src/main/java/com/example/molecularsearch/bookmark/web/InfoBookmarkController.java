d10983a73d7b712484eea412b3e07a7c98cd4fdd
DongminL <milkskfk12@gmail.com>
2024-10-01 00:35:44 +0900

    즐겨찾기 리스트 응답 DTO 추가

--- src/main/java/com/example/molecularsearch/bookmark/web/InfoBookmarkController.java
+++ src/main/java/com/example/molecularsearch/bookmark/web/InfoBookmarkController.java
@@ -1,6 +1,7 @@
 package com.example.molecularsearch.bookmark.web;
 
 import com.example.molecularsearch.bookmark.web.dto.InfoBookmarkDto;
+import com.example.molecularsearch.bookmark.web.dto.SearchBookmarkDto;
 import com.example.molecularsearch.exception.error.CustomException;
 import com.example.molecularsearch.exception.error.ErrorCode;
 import com.example.molecularsearch.bookmark.service.InfoBookmarkService;
@@ -35,7 +36,7 @@ public class InfoBookmarkController {
 
     /* 즐겨찾기 리스트 */
     @GetMapping("/api/search/bookmark")
-    public ResponseEntity<?> showInfoBookmark() {
+    public ResponseEntity<SearchBookmarkDto> showInfoBookmark() {
         return ResponseEntity.ok(infoBookmarkService.getBookmarkList());
     }
 
---
+++ src/main/java/com/example/molecularsearch/bookmark/web/dto/SearchBookmarkDto.java
@@ -0,0 +1,19 @@
+package com.example.molecularsearch.bookmark.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchBookmarkDto {
+
+    @JsonProperty("bookmarkList")
+    private List<InfoBookmarkDto> bookmarkDtoList;  // 즐겨찾기 목록
+}
